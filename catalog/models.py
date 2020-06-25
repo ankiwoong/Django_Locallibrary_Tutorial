52c38adcbe672204e9343054c8438a1ab88a3262
Ankiwoong <ankiwoong@gmail.com>
2020-06-25 15:38:15 +0900

    Defining the LocalLibrary Models - Genre model

--- catalog/models.py
+++ catalog/models.py
@@ -1,24 +1,13 @@
 from django.db import models
 
+# Create your models here.
+class Genre(models.Model):
+    """Model representing a book genre."""
 
-class MyModelName(models.Model):
-    """A typical class defining a model, derived from the Model class."""
-
-    # Fields
-    my_field_name = models.CharField(
-        max_length=20, help_text="Enter field documentation"
+    name = models.CharField(
+        max_length=200, help_text="Enter a book genre (e.g. Science Fiction)"
     )
-    ...
-
-    # Metadata
-    class Meta:
-        ordering = ["-my_field_name"]
-
-    # Methods
-    def get_absolute_url(self):
-        """Returns the url to access a particular instance of MyModelName."""
-        return reverse("model-detail-view", args=[str(self.id)])
 
     def __str__(self):
-        """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.my_field_name
+        """String for representing the Model object."""
+        return self.name
