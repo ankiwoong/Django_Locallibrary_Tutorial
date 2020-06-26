8df909dc23274fb090f417b9fd66ffad411cefe8
Ankiwoong <ankiwoong@gmail.com>
2020-06-26 09:20:08 +0900

    Configure list views

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,7 +10,7 @@ admin.site.register(Language)
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
 
 
 # Register the admin class with the associated model
@@ -20,7 +20,7 @@ admin.site.register(Author, AuthorAdmin)
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("title", "author", "display_genre")
 
 
 # Register the Admin classes for BookInstance using the decorator
