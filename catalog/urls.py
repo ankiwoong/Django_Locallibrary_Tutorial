29fcfd8f82c7e647b1ca3d64d585684651184684
Ankiwoong <ankiwoong@gmail.com>
2020-06-29 10:44:27 +0900

    View (class-based)

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("books/", views.BookListView.as_view(), name="books"),
-    # path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
     # re_path(r"^book/(?P<pk>\d+)$", views.BookDetailView.as_view(), name="book-detail"),
-    path("url/", views.my_reused_view, {"my_template_name": "some_path"}, name="aurl"),
-    path(
-        "anotherurl/",
-        views.my_reused_view,
-        {"my_template_name": "another_path"},
-        name="anotherurl",
-    ),
+    # path("url/", views.my_reused_view, {"my_template_name": "some_path"}, name="aurl"),
+    # path(
+    #     "anotherurl/",
+    #     views.my_reused_view,
+    #     {"my_template_name": "another_path"},
+    #     name="anotherurl",
+    # ),
 ]
