5ce0a510b739af5e6440477f81c9e9e72200163b
Ankiwoong <ankiwoong@gmail.com>
2020-06-29 14:36:08 +0900

    Pagination - Views

--- catalog/views.py
+++ catalog/views.py
@@ -31,6 +31,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the context
