4b2493466b87f483250618d25166dbe91f39726e
Ankiwoong <ankiwoong@gmail.com>
2020-07-06 15:21:53 +0900

    urls fix

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,25 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+
+APPS_PATTERNS = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("catalog/", include("catalog.urls")),
+]
+STATIC_PATTERN = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="catalog/", permanent=True)),
     path("admin/", admin.site.urls),
-    # Use include() to add paths from the catalog application
-    path("catalog/", include("catalog.urls")),
-    # Add URL maps to redirect the base URL to our application
-    path("", RedirectView.as_view(url="/catalog/", permanent=True)),
-] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-)  # Use static() to add url mapping to serve static files during development (only)
-
-# Add Django site authentication urls (for login, logout, password management)
-
-urlpatterns += [
-    path("accounts/", include("django.contrib.auth.urls")),
 ]
+urlpatterns += APPS_PATTERNS
+urlpatterns += STATIC_PATTERN
