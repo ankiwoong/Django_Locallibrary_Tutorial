50a7c597dc3ba428d926c20747e05012278ff50c
Ankiwoong <ankiwoong@gmail.com>
2020-07-06 15:55:35 +0900

    admin fix

--- catalog/admin.py
+++ catalog/admin.py
@@ -45,20 +45,12 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = (
-        "book",
-        "imprint",
-        "status",
-        "due_back",
-        "borrower",
-    )
-    list_filter = (
-        "status",
-        "due_back",
-    )
+    list_display = ("book", "status", "borrower", "due_back", "id")
+    list_filter = ("status", "due_back")
+
     fieldsets = (
-        (None, {"fields": ("book", "imprint", "uuid",),}),
-        ("Availability", {"fields": ("status", "due_back", "borrower",),}),
+        (None, {"fields": ("book", "imprint", "id")}),
+        ("Availability", {"fields": ("status", "due_back", "borrower")}),
     )
 
 
