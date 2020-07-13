42450bcbc287b2210531630acbfb7f41d3250c8a
Ankiwoong <ankiwoong@gmail.com>
2020-07-13 09:35:46 +0900

    catalog app - forms.py / code cleanup

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,52 +1,28 @@
-from django import forms
-import datetime
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-from django.forms import ModelForm
+import datetime  # for checking renewal date range.
 
-from catalog.models import BookInstance
+from django import forms
 
 
 class RenewBookForm(forms.Form):
+    """Form for a librarian to renew books."""
+
     renewal_date = forms.DateField(
         help_text="Enter a date between now and 4 weeks (default 3)."
     )
+
     # refer documentation > https://docs.djangoproject.com/en/2.1/ref/forms/fields/#datefield
 
     def clean_renewal_date(self):
         data = self.cleaned_data["renewal_date"]
 
-        # Check if a date is not in the past.
+        # Check date is not in past.
         if data < datetime.date.today():
             raise ValidationError(_("Invalid date - renewal in past"))
-
-        # Check if a date is in the allowed range (+4 weeks from today).
+        # Check date is in range librarian allowed to change (+4 weeks)
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(_("Invalid date - renewal more than 4 weeks ahead"))
 
         # Remember to always return the cleaned data.
         return data
-
-
-class RenewBookModelForm(ModelForm):
-    def clean_due_back(self):
-        data = self.cleaned_data["due_back"]
-
-        # Check if a date is not in the past.
-        if data < datetime.date.today():
-            raise ValidationError(_("Invalid date - renewal in past"))
-
-        # Check if a date is in the allowed range (+4 weeks from today).
-        if data > datetime.date.today() + datetime.timedelta(weeks=4):
-            raise ValidationError(_("Invalid date - renewal more than 4 weeks ahead"))
-
-        # Remember to always return the cleaned data.
-        return data
-
-    class Meta:
-        model = BookInstance
-        fields = ["due_back"]
-        labels = {"due_back": _("New renewal date")}
-        help_texts = {
-            "due_back": _("Enter a date between now and 4 weeks (default 3).")
-        }
